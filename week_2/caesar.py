821ab08ef4d896772cdc93795ae4dfb48ba547fd
전석준 <201402417>
2020-09-15 21:11:49 +0900

    week 2 homework

--- week_2/caesar.py
+++ week_2/caesar.py
@@ -12,4 +12,24 @@ def caesar_encrypt_decrypt(text: str, key: int) -> str:
     :param key: 암호화에 사용할 key
     :return: 시저 암호를 이용한 암호문 혹은 복호화된 문자열
     """
-    pass
+
+    strlist = list(text)
+    result = []
+
+    for j in range(len(text)):
+        if strlist[j] in lower_alphabet_list:
+            x = (ord(strlist[j]) - 97 + key) % 26
+            x += ord('a')
+            result.append(chr(x))
+
+        elif strlist[j] in upper_alphabet_list:
+            x = (ord(strlist[j]) - 65 + key) % 26
+            x += ord('A')
+            result.append(chr(x))
+
+        elif strlist[j] in number_list:
+            x = (ord(strlist[j]) - 48 + key) % 10
+            x += ord('0')
+            result.append(chr(x))
+
+    return('' . join(result))
--- week_2/vigenere.py
+++ week_2/vigenere.py
@@ -19,4 +19,59 @@ def vigenere_encrypt_decrypt(text: str, key: str, mode: EncryptionMode) -> str:
     :param mode: 암호화할 지 복호화할 지 구분하기 위한 값
     :return: 비제네르 암호를 이용한 암호문 혹은 복호화된 문자열
     """
-    pass
+    keylist = list(key)
+    strlist = list(text)
+
+    if len(text) == len(key):
+        keylist = list(key)
+    else:
+
+        for i in range(len(text) - len(key)):
+            keylist.append(keylist[i % len(key)])
+
+        '''
+        print(len(key_merged))
+        print(len(keylist))
+        '''
+
+    if mode == EncryptionMode.ENC:
+        enc_text = []
+
+        for j in range(len(text)):
+            if strlist[j] in lower_alphabet_list:
+                x = (ord(strlist[j]) + ord(keylist[j]) - 97 - 97) % 26
+                x += ord('a')
+                enc_text.append(chr(x))
+
+            elif strlist[j] in upper_alphabet_list:
+                x = (ord(strlist[j]) + ord(keylist[j]) - 65 - 97) % 26
+                x += ord('A')
+                enc_text.append(chr(x))
+
+            elif strlist[j] in number_list:
+                x = (ord(strlist[j]) + ord(keylist[j]) - 48 - 97) % 10
+                x += ord('0')
+                enc_text.append(chr(x))
+
+        return('' . join(enc_text))
+
+    elif mode == EncryptionMode.DEC:
+        dec_text = []
+
+        for i in range(len(text)):
+            if strlist[i] in lower_alphabet_list:
+                x = (ord(strlist[i]) - ord(keylist[i]) + 26) % 26
+                x += ord('a')
+                dec_text.append(chr(x))
+
+            elif strlist[i] in upper_alphabet_list:
+                x = (ord(strlist[i]) - ord(keylist[i]) + 32 + 26) % 26
+                x += ord('A')
+                dec_text.append(chr(x))
+
+            elif strlist[i] in number_list:
+                x = (ord(strlist[i]) - ord(keylist[i]) + 49 + 10) % 10
+                x += ord('0')
+                dec_text.append(chr(x))
+
+        return('' . join(dec_text))
