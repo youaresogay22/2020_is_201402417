2f2da0d27e712bc535da97f4ee842bf47176ec0d
전석준 <201402417>
2020-09-18 13:46:12 +0900

    week 3 homework

--- week_3/des.py
+++ week_3/des.py
@@ -3,26 +3,29 @@ from Crypto.Cipher import DES
 
 def encrypt_des(message: str, key: str, mode: int, iv=None) -> (bytes, bytes):
     """
+    pycrypto DES 라이브러리를 이용하여 주어진 평문을 암호화하는 함수
 
-    :param message:
-    :param key:
-    :param mode:
-    :param iv:
-    :return:
+    :param message: 암호화할 문자열
+    :param key: 암호화에 사용할 키값. 8바이트
+    :param mode: 암호화 시 어떤 모드로 동작할 지 설정. 기본값은 MODE_ECB
+    :param iv: 초기화 벡터(initialization vector), 블록 암호에서
+    첫 블록을 암호화할 때에 사용하는 문자열. MODE OFB에서의 길이는 8이며 기본값은 0
+    :return: DES cipher 객체를 이용해 암호화된 문자열과 이때 사용된 초기화 벡터
     """
-    if 
-    asd qwe
-    asd
-    pass
+
+    cipher = DES.new(bytes(key, 'utf-8'), mode, iv=iv)
+    return cipher.iv, cipher.encrypt(bytes(message, 'utf-8'))
 
 
 def decrypt_des(encrypted: bytes, key: str, mode: int, iv: bytes) -> str:
     """
-
-    :param encrypted:
-    :param key:
-    :param mode:
-    :param iv:
-    :return:
+    pycrypto DES 라이브러리를 이용하여 주어진 평문을 암호화하는 함수
+    :param encrypted: 암호화된 문자열
+    :param key: 복호화에 사용할 키값. 8바이트, 비밀 키.
+    :param mode: 암호화 시 어떤 모드로 동작할 지 설정
+    :param iv: 암호화 시 사용한 초기화 벡터(initialization vector)값, 모르면 복호화할 수 없다
+    :return: DES cipher 객체를 이용해 복호화된 문자열
     """
-    pass
+
+    cipher = DES.new(bytes(key, 'utf-8'), mode, iv=iv)
+    return cipher.decrypt(encrypted).decode('utf-8')
--- week_3/xor.py
+++ week_3/xor.py
@@ -14,11 +14,17 @@ def bits_to_string(bits: List[bool]) -> str:
 
 def xor_encrypt_decrypt(message: str, key: str) -> str:
     """
-
-    :param message:
-    :param key:
-    :return:
+    exclusive or 연산(^연산자)을 이용해 암호화 및 복호화를 수행하는 함수
+    파라미터로 받은 문자열을 비트로 변환한 뒤 
+    1비트마다 키의 비트 배열과 xor 연산을 계속 수행하여 암호화 또는 복호화
+    :param message: 암호화 또는 복호화할 문자열
+    :param key: 암호화 또는 복호화에 이용할 key 값
+    :return: exclusive or 연산을 통해 암호화 또는 복호화된 문자열
     """
     message_bits = string_to_bits(message)
     key_bits = string_to_bits(key)
-    pass
+
+    for i in range(len(message_bits)):
+        message_bits[i] = message_bits[i] ^ key_bits[i % len(key_bits)]
+
+    return bits_to_string(message_bits)
