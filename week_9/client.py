3797f2c1442d94ef93a9b2a66ed46965bb025c0b
전석준 <201402417>
2020-11-07 02:00:23 +0900

    week9 homework

--- week_9/client.py
+++ week_9/client.py
@@ -44,20 +44,23 @@ def receive(sock, key):
 def login(socket):
     """
     TODO: 아이디와 패스워드 값을 전송하는 함수
-    :return:
+    만들어진 send 못쓴이유: key가 없어서
+    :return: 없음
     """
-    user_id = input()
-    password = input()
+    user_id = input('id: ')
+    password = input('pw: ')
     # socket 통신으로 id와 password를 전송
-    pass
+    data = (user_id + "/"+password).encode('utf-8')
+    socket.send(data)
 
 
 def generate_key(key: int or None = None) -> int:
     """
     테스트를 위해 임의 key를 입력할 경우에 해당 key를 반환하도록 구현
     기본적으로 랜덤한 secret 키를 생성
-    :param key:
-    :return:
+    :param key: 반환받을 키값
+    :return: 랜덤 키 생성 혹은 입력값 그대로 반환
+    저는 테스트 안해봐서 모르겠습니다. 필요한가?
     """
     if key is not None:
         return key
@@ -66,24 +69,31 @@ def generate_key(key: int or None = None) -> int:
 
 def diffie_hellman(my_secret_key: int, target_public_key: int) -> bytes:
     """
-    TODO: [함수 설명] 16byte 크기로 잘라 반
-    :param my_secret_key:
-    :param target_public_key:
-    :return:
+    TODO: [함수 설명] 16byte 크기로 잘라 반 <-?
+    설명이 뭔지 몰라서
+    생성된 키를 byte 변환한 것의 앞 16바이트 잘라서 사용
+    
+    :param my_secret_key: 내 개인키
+    :param target_public_key: 상대방 공개키
+    :return: 상대방 공개키** 내 개인키 (mod p) 값의 앞 16바이트
     """
     p = 9723448991222331098330293371197519246446906995517093957384966642329511534161627658859950763542683697458467770974347360725590854862427735703874399411721649
     g = 2348329574892572380947382043
+    original_key = pow(target_public_key, my_secret_key, p)
+    key = original_key.to_bytes(1024, byteorder='little')
+
+    return key[:16]
 
 
 def public_key(secret_key: int) -> int:
     """
     TODO: diffie hellman에서의 public key를 계산하여 반환하는 함수
-    :param secret_key:
-    :return:
+    :param secret_key: 내 개인키
+    :return: 전송할 공개키
     """
     p = 9723448991222331098330293371197519246446906995517093957384966642329511534161627658859950763542683697458467770974347360725590854862427735703874399411721649
     g = 2348329574892572380947382043
-
+    return pow(g, secret_key, p)
 
 def connect_socket():
     port = 8081
@@ -97,6 +107,15 @@ def connect_socket():
 
     my_secret_key = generate_key()
     # TODO: 자신의 public key를 전송해야 함
+    '''
+    키교환 코드 부분이 자연스럽게 실행되게 하면 서버에서 인식오류가 나서
+    116번째 라인 빈 변수를 사용하여 임의로 통신흐름을 제어함
+    더 깔끔하게 할 방법은 모르겠음
+    이정도면 굉장히 노력했다고 생각합니다.
+    '''
+    dunnohowtostop = input('press enter to send key')
+    my_pub_key = public_key(my_secret_key)
+    client_socket.send(my_pub_key.to_bytes(1024, byteorder='little'))
 
     target_public_key = int.from_bytes(client_socket.recv(1024), byteorder='little')  # little endian 으로 보내야 함
     key = diffie_hellman(my_secret_key, target_public_key)
--- week_9/server.py
+++ week_9/server.py
@@ -38,12 +38,28 @@ def load_db():
 def verify_login(db, user_id, password) -> bool:
     """
     TODO: db에 있는 user id와 password의 해시값을 통해 입력받은 id와 password 값이 옳은 지 검증
-    로그인 실패 시
-    :param db:
-    :param user_id:
-    :param password:
-    :return:
+    로그인 실패 시 <-?
+    실패 시 이 함수에서는 거짓값만 리턴. 
+
+    :param db: 데이터베이스... List of dictionaries 객체
+    :param user_id: 통신으로 입력받은 id
+    :param password: 통신으로 입력받은 password
+    :return: 로그인 성공하면 true, 실패시 false
     """
+    db = load_db()
+    dick = {}
+
+    for i in db:
+        if i['id'] == user_id:
+            dick = i
+
+    db_hash_val = dick['password']
+    user_hash_val = sha256(password.encode('utf-8')).hexdigest()
+
+    if db_hash_val == user_hash_val:
+        return True
+    else:
+        return False
 
 
 def connect_socket():
@@ -63,8 +79,31 @@ def connect_socket():
 
     # TODO: 두 클라이언트의 login 확인
     # 한 클라이언트라도 로그인 실패 시 server_socket.close() 호출 후 종료
+    client1_recv = connection_socket1.recv(1024)
+    client1_recv_decode = client1_recv.decode('utf-8')
+    client1_id = client1_recv_decode.split('/')[0]
+    client1_pw = client1_recv_decode.split('/')[1]
+
+    client2_recv = connection_socket2.recv(1024)
+    client2_recv_decode = client2_recv.decode('utf-8')
+    client2_id = client2_recv_decode.split('/')[0]
+    client2_pw = client2_recv_decode.split('/')[1]
+
+    client_auth_1 = verify_login(db, client1_id, client1_pw)
+    client_auth_2 = verify_login(db, client2_id, client2_pw)
+
+    if client_auth_1 and client_auth_2:
+         print('login success')
+    else:
+        print('login failure')
+        server_socket.close()
+        exit(0)
 
     # TODO: 두 클라이언트 public key 전달
+    client1_recv_pubkey = connection_socket1.recv(1024)
+    send(connection_socket2, client1_recv_pubkey)
+    client2_recv_pubkey = connection_socket2.recv(1024)
+    send(connection_socket1, client2_recv_pubkey)
 
     receiver1 = threading.Thread(target=receive, args=(connection_socket1, addr1, connection_socket2))
     receiver2 = threading.Thread(target=receive, args=(connection_socket2, addr2, connection_socket1))
